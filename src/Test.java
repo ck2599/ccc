85842e24583ef7cab49b3bbd9e6227642ab13625
大数据12_李志康 <1519389876@qq.com>
2019-07-26 14:22:28 +0800

    helloworld

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @author 1519389876@qq.com
+ * @version v 1.0
+ * @date 2019/7/26  11:22
+ * @infos
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("helloworld！！！");
+    }
+}
